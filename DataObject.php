a14e8b23c20717531356dc358f47d33c463efca5
Vitaliy Orlov <orlov0562@gmail.com>
2014-12-31 09:55:15 +0200

    Unused logic removed

--- DataObject.php
+++ DataObject.php
@@ -1,55 +1,46 @@
 <?php
 
     class DataObject {
-        private $data = array();
+        
+        private $_data = array();
 
         public function loadData(array $data) {
-            $this->data = $data;
+            $this->_data = $data;
         }
         
         public function appendData(array $data) {
-            $this->data = array_merge($this->data, $data);
+            $this->_data = array_merge($this->_data, $data);
         }
 
         public function getData() {
-            return $this->data;
+            return $this->_data;
         }
 
-        public function __call($name, array $arguments)
-        {
-            return property_exists(__CLASS__, $name)
-                   ? $this->$name($arguments)
-                   : $this->_call($name, $arguments)
-            ;
+        public function getDataWithoutKeys(array $keys) {
+            return array_diff_key($this->_data, array_fill_keys($keys, null));
         }
-        
-        private function _call($name, array $arguments) {
+
+        public function __call($method, array $arguments)
+        {
             $ret = null;
-            switch( $this->_getPrefix($name) ) {
-                default:
-                    throw $this->getMethodNotFoundException();        
-                break;
-                case 'get':
-                    $ret = $this->_get($name, $arguments);
-                break;
-                case 'set':
-                    $this->_set($name, $arguments);
-                break;
-                case 'uns':
-                    $this->_uns($name);
-                break;                
+            $prefix = $this->_getPrefix($method);
+            if (!in_array($prefix, array('get', 'set', 'uns'))) {
+                throw $this->getMethodNotFoundException();
+            } else {
+                $var = $this->_getVarName($method);
+                $ret = $this->{'_'.$prefix}($var, $arguments);
             }
             return $ret;        
         }
         
+        private function _getPrefix($name) { 
+            return substr($name, 0, 3); 
+        }
+                
         protected function getMethodNotFoundException() {
             return new Exception('Method not found');    
         }
 
-        private function _getPrefix($name) { 
-            return substr($name, 0, 3); 
-        }
-        
         private function _getVarName($name) {
             $varName = substr($name, 3);
             $varName = preg_replace('~([a-z0-9])([A-Z])~', '$1_$2', $varName);
@@ -57,24 +48,22 @@
             return $varName; 
         }
         
-        private function _get($name, array $arguments) {
-            $varName = $this->_getVarName($name);
-            return isset($this->data[$varName])
-                   ? $this->data[$varName]
+        private function _get($var, array $arguments) {
+            
+            return isset($this->_data[$var])
+                   ? $this->_data[$var]
                    : ( isset($arguments[0]) ? $arguments[0] : null );        
         }
 
-        private function _set($name, array $arguments) {
-            $varName = $this->_getVarName($name);
-            $this->data[$varName] = isset($arguments[0])
+        private function _set($var, array $arguments) {
+            $this->_data[$var] = isset($arguments[0])
                                     ? $arguments[0]
                                     : null
             ;
         }
         
-        private function _uns($name) {
-            $varName = $this->_getVarName($name);
-            if (isset($this->data[$varName])) unset($this->data[$varName]);
+        private function _uns($var) {
+            if (isset($this->_data[$var])) unset($this->_data[$var]);
         }
         
     }
